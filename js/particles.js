6ea2f7d63b2a5f455ccec216db436c3cf8066ada
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2024-11-18 22:01:05 +0000

    push 3

---
+++ js/particles.js
@@ -0,0 +1,41 @@
+// particles.js config (particles.js)
+
+particlesJS('particles-js', {
+    "particles": {
+        "number": {
+            "value": 100,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "size": {
+            "value": 3,
+            "random": true
+        },
+        "move": {
+            "enable": true,
+            "speed": 2,
+            "direction": "none",
+            "random": true,
+            "straight": false,
+            "out_mode": "out",
+            "attract": {
+                "enable": false
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "repulse"
+                },
+                "onclick": {
+                    "enable": true,
+                    "mode": "push"
+                }
+            }
+        }
+    }
+});
---
+++ js/slick-carousel.js
@@ -0,0 +1,14 @@
+// slick-carousel.js
+
+$(document).ready(function() {
+    // Initialize Slick Carousel
+    $('.slick-slider').slick({
+        arrows: false,
+        loop: true,
+        dots: true,
+        autoplay: true,
+        fade: true,
+        speed: 1000,
+        autoplaySpeed: 3000
+    });
+});
