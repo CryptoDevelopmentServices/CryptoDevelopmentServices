75ae91812e048863103207cc70d6351b697df3dd
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2024-11-19 21:55:17 +0000

    push 4

--- js/particles.js
+++ js/particles.js
@@ -6,7 +6,7 @@ particlesJS('particles-js', {
             "value": 100,
             "density": {
                 "enable": true,
-                "value_area": 800
+                "value_area": 900
             }
         },
         "size": {
